df494cfb72137bc5ca4a8609c12b61406dc399e0
Alagbe Alim <your.alagbealim81@gmail.com>
2023-09-23 23:40:48 +0100

    ...

--- src/components/Fast-Easy-Section.js
+++ src/components/Fast-Easy-Section.js
@@ -15,43 +15,44 @@ const FastEasySection = () => {
     width: "190px",
   };
   return (
-    <Container p={5} height="80vh" display={"flex"}>
-      <Flex
-        direction={"column"}
-        justifyContent={"space-evenly"}
-        alignItems={"center"}
-        margin={"20px"}
-      >
-        <Flex direction={"row"} gap={6}>
-          <Box width={"320px"} height={"240px"} alignItems={"center"}>
-            <Image borderRadius={"20px"} src="./fast.jpg" />
-          </Box>
-          <Box {...boxStyles}>
-            <Heading mb={3} color="#333">
-              Fast
-            </Heading>
-            <Text fontSize="lg" color={"#888"} mb={5}>
-              You've never made a class related content this fast before.
-            </Text>
-            <ReadMore color="rgb(255, 212, 0)" bg="rgb(34, 34, 34)" />
-          </Box>
-        </Flex>
-        <Flex direction={"row"} gap={6}>
-          <Box width={"320px"} height={"240px"} alignItems={"center"}>
-            <Image borderRadius={"20px"} src="./easy.jpg" />
-          </Box>
-          <Box {...boxStyles}>
-            <Heading mb={3} color="#333">
-              Easy
-            </Heading>
-            <Text fontSize="lg" color={"#888"} mb={5}>
-              Works with standards and styles you're familiar with.
-            </Text>
-            <ReadMore color="rgb(255, 212, 0)" bg="rgb(34, 34, 34)" />
-          </Box>
-        </Flex>
+    <Box
+      p={6}
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      bg={"white"}
+      width={"100%"}
+      gap={6}
+    >
+      <Flex direction={"row"}>
+        <Box width={"300px"} height={"240px"} alignItems={"center"}>
+          <Image borderRadius={"20px"} src="./fast.jpg" />
+        </Box>
+        <Box width={"190px"}>
+          <Heading mb={3} color="#333">
+            Fast
+          </Heading>
+          <Text fontSize="lg" color={"#888"} mb={5}>
+            You've never made a class related content this fast before.
+          </Text>
+          <ReadMore color="rgb(255, 212, 0)" bg="rgb(34, 34, 34)" />
+        </Box>
       </Flex>
-    </Container>
+      <Flex direction={"row"}>
+        <Box width={"300px"} height={"240px"} alignItems={"center"}>
+          <Image borderRadius={"20px"} src="./easy.jpg" />
+        </Box>
+        <Box width={"190px"}>
+          <Heading mb={3} color="#333">
+            Easy
+          </Heading>
+          <Text fontSize="lg" color={"#888"} mb={5}>
+            Works with standards and styles you're familiar with.
+          </Text>
+          <ReadMore color="rgb(255, 212, 0)" bg="rgb(34, 34, 34)" />
+        </Box>
+      </Flex>
+    </Box>
   );
 };
 
