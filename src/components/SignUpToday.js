8fadc7937d834f923570cfbd1710e28b37c00317
Alim <alagbealim81@gmail.com>
2023-09-24 05:40:50 +0000

    responsiveness...

--- src/components/SignUpToday.js
+++ src/components/SignUpToday.js
@@ -5,27 +5,31 @@ import GetStarted from "./buttons/GetStarted";
 
 const SignUpToday = () => {
   return (
-    <Container height={"20vh"} display={"flex"} alignItems={"center"} p={4}>
-      <Box alignItems={"center"} width={"100%"}>
-        <Flex
-          direction={"column"}
-          alignItems={"center"}
-          justifyContent={"center"}
-        >
-          <Heading fontSize={"50px"} mb={5}>
-            Sign up today.
-          </Heading>
-          <Flex gap={4}>
-            <LearnMore height="40px" bg="rgb(235, 235, 235)" color="black" />
-            <GetStarted
-              height="40px"
-              color="rgb(255, 212, 0)"
-              bg="rgb(34, 34, 34)"
-            />
-          </Flex>
+    <Box
+      bg={"white"}
+      h={{ base: "50vh", sm: "20vh" }}
+      alignItems={"center"}
+      width={"100%"}
+      p={5}
+    >
+      <Flex
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Heading fontSize={"50px"} mb={5}>
+          Sign up today.
+        </Heading>
+        <Flex gap={4}>
+          <LearnMore height="40px" bg="rgb(235, 235, 235)" color="black" />
+          <GetStarted
+            height="40px"
+            color="rgb(255, 212, 0)"
+            bg="rgb(34, 34, 34)"
+          />
         </Flex>
-      </Box>
-    </Container>
+      </Flex>
+    </Box>
   );
 };
 
