d92e5db18e228f52274f8771d2e9f0caba1243b3
Joye Shonubi <joye@goforward.com>
2023-10-29 17:47:49 -0700

    fix nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 "use client";
 import React from "react";
-import { Box, Flex, Image, Link as ChakraLink } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Image,
+  Link as ChakraLink,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import SignUp from "./buttons/SignUp";
 
 const Navbar = () => {
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+  console.log("JOYEEEEEEEE", isLargerThan768);
+  const desktopLogo = "./wide-logo.svg";
+  const mobileLogo = "./mobile-logo.svg";
   return (
     <Box width={"100%"} bg="white">
       <Flex
@@ -14,7 +24,11 @@ const Navbar = () => {
         alignItems={"center"}
       >
         <Box width={{ base: "130px", sm: "250px" }}>
-          <Image src="./logo1.jpg" alt="logo" />
+          {isLargerThan768 ? (
+            <Image objectFit={"cover"} src="/mobile-logo.svg" />
+          ) : (
+            <Image objectFit={"cover"} src="/wide-logo.svg" />
+          )}
         </Box>
         <ChakraLink href="https://app.teachassist.ai/register" isExternal>
           <SignUp fontSize="14px" />
